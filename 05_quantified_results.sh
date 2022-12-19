986d60ace89d4896230184293b995791c4cab17b
Pourya Shirazian <Pourya.Shirazian@intusurg.com>
2022-12-19 12:43:31 -0800

    adds the quantified results script

--- 05_quantified_results.sh
+++ 05_quantified_results.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-
-python eval_rgb.py --gt_dir /path/to/data/images --mask_dir /path/to/data/gt_masks --img_dir logs/{expname}/renderonly_path_fixidentity_{epoch}/estim/
+python eval_rgb.py \
+    --gt_dir dataset/pulling_soft_tissues/images \
+    --mask_dir dataset/pulling_soft_tissues/gt_masks \
+    --img_dir experiments/pulling_soft_tissues/logs/pulling_soft_tissues/renderonly_path_fixidentity_100000/estim/
